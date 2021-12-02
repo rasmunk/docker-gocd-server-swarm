6a9e4db0e65e671e1e340c51841dc1ffed6f23df
Rasmus Munk <munk1@live.dk>
2021-12-02 21:13:11 +0100

    Fix path typo

--- utils/defaults.py
+++ utils/defaults.py
@@ -7,6 +7,6 @@ default_config_path = os.path.join(default_base_path, "config")
 
 cluster_profiles_path = os.path.join(default_config_path, "cluster_profiles.yml")
 elastic_agent_profile_path = os.path.join(
-    default_config_path, "elastic_agent_profile.yml"
+    default_config_path, "elastic_agent_profiles.yml"
 )
 repositories_path = os.path.join(default_config_path, "repositories.yml")
