88a9baafb37ecdaba303f3f0f2218b95812db28b
Rasmus Munk <munk1@live.dk>
2021-12-05 09:15:48 +0100

    Start of the setup_gocd_server simplification refactor

--- utils/defaults.py
+++ utils/defaults.py
@@ -12,3 +12,18 @@ elastic_agent_profile_path = os.path.join(
 repositories_path = os.path.join(default_config_path, "repositories.yml")
 authorization_config_path = os.path.join(default_base_path, "authorization_config.yml")
 secret_managers_config_path = os.path.join(default_base_path, "secret_managers.yml")
+
+
+# API Request Defaults
+CONTENT_TYPE = "application/json"
+JSON_HEADER = {"Content-Type": CONTENT_TYPE}
+
+API_VERSION_1 = "application/vnd.go.cd.v1+json"
+API_VERSION_2 = "application/vnd.go.cd.v2+json"
+API_VERSION_3 = "application/vnd.go.cd.v3+json"
+API_VERSION_4 = "application/vnd.go.cd.v4+json"
+
+ACCEPT_HEADER_1 = {"Accept": API_VERSION_1, **JSON_HEADER}
+ACCEPT_HEADER_2 = {"Accept": API_VERSION_2, **JSON_HEADER}
+ACCEPT_HEADER_3 = {"Accept": API_VERSION_3, **JSON_HEADER}
+ACCEPT_HEADER_4 = {"Accept": API_VERSION_4, **JSON_HEADER}
--- utils/setup_gocd_server.py
+++ utils/setup_gocd_server.py
@@ -7,23 +7,30 @@ from defaults import (
     cluster_profiles_path,
     elastic_agent_profile_path,
     repositories_path,
-    secret_managers_config_path
+    secret_managers_config_path,
+    JSON_HEADER,
+    ACCEPT_HEADER_1,
+    ACCEPT_HEADER_2,
+    ACCEPT_HEADER_3,
+    ACCEPT_HEADER_4
 )
 from config import load_config
 
 
-CONTENT_TYPE = "application/json"
-
 if "BASE_URL" in os.environ:
     BASE_URL = os.environ["BASE_URL"]
 else:
-    BASE_URL = ""
+    BASE_URL = "https://mechanist.rasmunk.dk"
 
+# Public URLs
 GO_URL = "{}/go".format(BASE_URL)
 API_URL = "{}/api".format(GO_URL)
 
+AUTH_URL = "{}/current_user".format(API_URL)
 ELASTIC_AGENT_URL = "{}/elastic/profiles".format(API_URL)
 ADMIN_URL = "{}/admin".format(API_URL)
+
+# Restricted URLs
 CLUSTER_PROFILES_URL = "{}/elastic/cluster_profiles".format(ADMIN_URL)
 CONFIG_REPO_URL = "{}/config_repos".format(ADMIN_URL)
 SECRET_CONFIG_URL = "{}/secret_configs".format(ADMIN_URL)
@@ -31,25 +38,11 @@ SECRET_CONFIG_URL = "{}/secret_configs".format(ADMIN_URL)
 if "AUTH_TOKEN" in os.environ:
     AUTH_TOKEN = os.environ["AUTH_TOKEN"]
 else:
-    AUTH_TOKEN = ""
+    AUTH_TOKEN = "e657c2526dd045c16a0a18104983073044aad222"
     # The AUTH_TOKEN is the one generate within the GOCD server
     # (Not GitHub)
 
-
-
-def authenticate(session, headers=None):
-    if not headers:
-        headers = {
-            "Authorization": "bearer {}".format(AUTH_TOKEN),
-            "Accept": "application/vnd.go.cd.v1+json",
-        }
-    auth_url = "{}/current_user".format(API_URL)
-    resp = session.get(auth_url, headers=headers)
-    if resp.status_code == 200:
-        return True
-    print(resp.text)
-    return False
-
+AUTHORIZATION_HEADER = {"Authorization": "bearer {}".format(AUTH_TOKEN), **ACCEPT_HEADER_1}
 
 def get(session, url, *args, **kwargs):
     try:
@@ -66,6 +59,31 @@ def post(session, url, *args, **kwargs):
         print("Failed POST request: {}".format(err))
     return None
 
+
+def get_type(session, base_url, id, headers=None):
+    id_url = "{}/{}".format(base_url, id)
+    resp = get(session, id_url, headers=headers)
+    if resp.status_code == 200:
+        return resp.text
+    return None
+
+
+def get_types(session, base_url, headers=None):
+    resp = get(session, base_url, headers=headers)
+    if resp.status_code == 200:
+        return resp.text
+    return None
+
+def create_type(session, url, data=None, headers=None):
+    json_data = json.dumps(data)
+    return post(session, url, data=json_data, headers=headers).text
+
+
+def authenticate(session):
+    resp = get(session, AUTH_URL, headers=AUTHORIZATION_HEADER)
+    if resp.status_code == 200:
+        return True
+    return False
 
 def get_cluster_profiles(session, headers=None):
     if not headers:
@@ -169,31 +187,16 @@ def get_secret(auth_data):
 def get_repo_secret_manager(repository_config):
     return repository_config["authentication"]["secret_plugin"]
 
-def get_secret_config(session, id, headers=None):
-    if not headers:
-        headers = {"Accept": "application/vnd.go.cd.v4+json"}
-    id_url = "{}/{}".format(SECRET_CONFIG_URL, id)
-    resp = get(session, id_url, headers=headers)
-    if resp.status_code == 200:
-        return resp.text
-    return None
-
 
 def get_secret_configs(session, headers=None):
     if not headers:
-        headers = {
-            "Accept": "application/vnd.go.cd.v3+json",
-            "Content-Type": CONTENT_TYPE
-        }
+        headers = ACCEPT_HEADER_3
     return get(session, SECRET_CONFIG_URL)
 
 
 def create_secret_config(session, data=None, headers=None):
     if not headers:
-        headers = {
-            "Accept": "application/vnd.go.cd.v3+json",
-            "Content-Type": CONTENT_TYPE,
-        }
+        headers = ACCEPT_HEADER_3
     if not data:
         data = {}
     json_data = json.dumps(data)
@@ -229,11 +232,14 @@ if __name__ == "__main__":
 
 #        print("Setup Authorization config")
 
+
         print("Setup Secret Manager")
         for secret_manager_config in secret_managers_config:
-            exists = get_secret_config(session, secret_manager_config["id"])
+            exists = get_type(session, SECRET_CONFIG_URL, secret_manager_config["id"], headers=ACCEPT_HEADER_4)
+#            exists = get_secret_config(session, secret_manager_config["id"])
             if not exists:
-                created = create_secret_config(session, data=secret_manager_config)
+                created = create_type(session, SECRET_CONFIG_URL, data=secret_manager_config, headers=ACCEPT_HEADER_3)
+                # created = create_secret_config(session, data=secret_manager_config)
                 if not created:
                     print("Failed to create secret config: {}".format(
                         secret_manager_config
@@ -242,9 +248,11 @@ if __name__ == "__main__":
 
         print("Setup Cluster profiles")
         # Create cluster profile
-        existing_cluster = get_cluster(session, cluster_profiles_config["id"])
+        #existing_cluster = get_cluster(session, cluster_profiles_config["id"])
+        existing_cluster = get_type(session, CLUSTER_PROFILES_URL, cluster_profiles_config["id"], headers=ACCEPT_HEADER_1)
         if not existing_cluster:
-            created = create_cluster_profile(session, data=cluster_profiles_config)
+            #created = create_cluster_profile(session, data=cluster_profiles_config)
+            created = create_type(session, CLUSTER_PROFILES_URL, data=cluster_profiles_config, headers=ACCEPT_HEADER_1)
             if not created:
                 print(
                     "Failed to create cluster profile: {}".format(
@@ -253,16 +261,19 @@ if __name__ == "__main__":
                 )
                 exit(4)
 
-        existing_agent = get_elastic_agent(session, elastic_agent_config["id"])
+        #existing_agent = get_elastic_agent(session, elastic_agent_config["id"])
+        existing_agent = get_type(session, ELASTIC_AGENT_URL, elastic_agent_config["id"], headers=ACCEPT_HEADER_2)
         if not existing_agent:
-            created = create_elastic_agent_profile(session, data=elastic_agent_config)
+            #created = create_elastic_agent_profile(session, data=elastic_agent_config)
+            created = create_type(session, ELASTIC_AGENT_URL, data=elastic_agent_config, headers=ACCEPT_HEADER_2)
             if not created:
                 print("Failed to create elastic agent profile: {}".format(created))
                 exit(5)
 
         print("Create Config Repositories")
         for repository_config in repositories_config:
-            existing_repo = get_config_repo(session, repository_config["id"])
+            #existing_repo = get_config_repo(session, repository_config["id"])
+            existing_repo = get_type(session, CONFIG_REPO_URL, repository_config["id"], headers=ACCEPT_HEADER_4)
             if not existing_repo:
                 # Check whether a secret auth token is required
                 extra_config_kwargs = {}
@@ -271,6 +282,7 @@ if __name__ == "__main__":
                     secret_manager = get_secret_manager(repository_config)
                     secret = get_secret(auth_data)
                     if not secret:
+                        #created = create_secret(repository_config)
                         created = create_secret(repository_config)
                         if not created:
                             print("Failed to create new secret")
