236a704d1c6847555d07bcf438a24bd7046aafc2
Rasmus Munk <munk1@live.dk>
2021-12-15 20:25:02 +0100

    Update how the secrets script processes the output

--- docker-entrypoint.d/secrets.py
+++ docker-entrypoint.d/secrets.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 import subprocess
+import inspect
 import json
+from ast import literal_eval
 
 
-def run_command(command):
-    result = subprocess.run(init_command)
+def run_command(command, execute_kwargs=None):
+    run_kwargs = {}
+    available_arguments = inspect.getfullargspec(subprocess.run)
+    if "capture_output" in available_arguments.kwonlyargs:
+        run_kwargs["capture_output"] = execute_kwargs["capture"]
+    else:
+        if execute_kwargs["capture"]:
+            run_kwargs["stdout"] = subprocess.PIPE
+            run_kwargs["stderr"] = subprocess.PIPE
+
+    result = subprocess.run(command, **run_kwargs)
     command_results = {}
     if hasattr(result, "args"):
         command_results.update({"command": " ".join((getattr(result, "args")))})
@@ -17,21 +28,41 @@ def run_command(command):
     return command_results
 
 
+def format_output_json(result):
+    json_result = {}
+    for key, value in result.items():
+        if key != "command":
+            evalued = literal_eval(value)
+            if isinstance(evalued, bytes):
+                evalued = evalued.decode("utf-8")
+            if isinstance(evalued, str):
+                # Ensure utf-8 encoding
+                evalued = evalued.encode("utf-8")
+            if isinstance(evalued, int):
+                evalued = str(evalued)
+
+            if len(evalued) > 0:
+                json_result[key] = json.loads(evalued)
+            else:
+                json_result[key] = value
+    return json_result
+
+
 if __name__ == "__main__":
     print("Run gocd-tools secrets init")
     init_command = ["gocd-tools", "setup", "secrets", "init"]
-    result = run_command(init_command)
+    result = run_command(init_command, execute_kwargs={"capture": True})
 
-    init_output_json = json.dumps(result["output"])
-    if init_output_json["status"] != "success":
+    json_result = format_output_json(result)
+    if len(json_result["error"]) > 0:
         print("Failed command: {}".format(init_command))
-        print(init_output_json)
+        print(json_result["error"]["msg"])
 
     print("Run gocd-tools secrets configure")
     configure_command = ["gocd-tools", "setup", "secrets", "configure"]
     conf_result = run_command(configure_command)
 
-    conf_output_json = json.dumps(conf_result["output"])
-    if conf_output_json["status"] != "success":
-        print("Failed command: {}".format(configure_command))
-        print(conf_output_json)
+    conf_output_json = format_output_json(conf_result)
+    if len(json_result["error"]) > 0:
+        print("Failed command: {}".format(init_command))
+        print(json_result["error"]["msg"])
