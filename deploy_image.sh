80f0cc59a2e68ba1b9c3bd3c0e3ecc0858ee52e2
Rasmus Munk <munk1@live.dk>
2022-01-10 13:22:31 +0100

    Make sure both the tagged and default tag images are deployed by gocd

--- deploy_image.sh
+++ deploy_image.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-TAG=$1
+IMAGE_TAG=$1
+DEFAULT_TAG=$2
 
 if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
-make build TAG=${TAG}
-make push TAG=${TAG}
+# Special tag
+make build TAG=${IMAGE_TAG}
+make push TAG=${IMAGE_TAG}
+
+# Default tag
+make build TAG=${DEFAULT_TAG}
+make push TAG=${DEFAULT_TAG}
