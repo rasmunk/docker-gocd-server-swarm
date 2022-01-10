c8af584d879d64c364dce77521749e128ac383dc
Rasmus Munk <munk1@live.dk>
2022-01-10 12:48:25 +0100

    Update the gocd config

---
+++ deploy_image.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TAG=$1
+
+if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
+    echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
+fi
+
+make build TAG=${TAG}
+make push TAG=${TAG}
